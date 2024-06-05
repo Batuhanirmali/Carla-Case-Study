584547db91ac0daa6157a7a47b4cd30210f5541c
Batuhanirmali <batuqwert1@gmail.com>
2024-06-05 12:30:33 +0300

    Initial Commit

---
+++ CarlaCaseStudy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CarlaCaseStudy
+//
+//  Created by Talha Batuhan Irmalı on 5.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
